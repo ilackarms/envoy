9191b69c9d81f0e5645738edad8db1bb235a70ee
ilackarms <sdw35@cornell.edu>
2018-03-19 19:21:31 -0400

    add option to skip clearing the route cache in grpc transcoder filter

--- source/common/grpc/json_transcoder_filter.h
+++ source/common/grpc/json_transcoder_filter.h
@@ -62,6 +62,14 @@ public:
                    std::unique_ptr<google::grpc::transcoding::Transcoder>& transcoder,
                    const Protobuf::MethodDescriptor*& method_descriptor);
 
+
+  /**
+   * If true, skip clearing the route cache after the incoming request has been modified.
+   * This allows Envoy to select the upstream cluster based on the incoming request
+   * rather than the outgoing.
+   */
+   bool skip_clearing_route_cache();
+
 private:
   /**
    * Convert method descriptor to RequestInfo that needed for transcoding library
@@ -74,6 +82,8 @@ private:
   google::grpc::transcoding::PathMatcherPtr<const Protobuf::MethodDescriptor*> path_matcher_;
   std::unique_ptr<google::grpc::transcoding::TypeHelper> type_helper_;
   Protobuf::util::JsonPrintOptions print_options_;
+
+  bool skip_clearing_route_cache_{false};
 };
 
 typedef std::shared_ptr<JsonTranscoderConfig> JsonTranscoderConfigSharedPtr;
